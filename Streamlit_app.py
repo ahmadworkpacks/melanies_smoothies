9fded3ed3f0502c9808c9baa2e7ded807d1e24e4
ahmadworkpacks <146561308+ahmadworkpacks@users.noreply.github.com>
2024-05-17 17:35:41 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -20,6 +20,8 @@ pd_df=my_dataframe.to_pandas()
 st.dataframe(data=pd_df, use_container_width=True)
 st.stop()
 ingredientes_list=st.multiselect('Choose up to 5 ingredients',my_dataframe,max_selections=5)
+search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 if ingredientes_list:
     ingredients_string=' '
     for fruit in ingredientes_list:
